b2b515c9335193aeabcaa813750077920caa6a27
unknown <wahadisllahi@gmail.com>
2025-04-22 08:20:09 -0700

    A1we

--- app/courses/[id]/page.tsx
+++
@@ -1,5 +0,0 @@
-// import CoursePage from "@/components/courses/course-page";
-
-// export default function Page({ params }: { params: { id: string } }) {
-//   return <CoursePage params={params} />;
-// }
