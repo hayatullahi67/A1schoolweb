d8fda5d1c3a443f539595fe53eb7fa137eedd1b2
unknown <wahadisllahi@gmail.com>
2025-04-22 08:16:18 -0700

    A1we

--- app/courses/[id]/page.tsx
+++ app/courses/[id]/page.tsx
@@ -1,5 +1,5 @@
-import CoursePage from "@/components/courses/course-page";
+// import CoursePage from "@/components/courses/course-page";
 
-export default function Page({ params }: { params: { id: string } }) {
-  return <CoursePage params={params} />;
-}
+// export default function Page({ params }: { params: { id: string } }) {
+//   return <CoursePage params={params} />;
+// }
